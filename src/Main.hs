9c69042222b0f30eb58d7bc4d03f1993441157f4
ncaq <ncaq@ncaq.net>
2015-05-30 21:33:58 +0900

    formated: indent

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,9 @@ main = toJSONFilter include
 
 include :: Block -> IO Block
 include cb@(CodeBlock (identity, classes, namevals) _) =
-    case lookup "include" namevals of
-     Just f  -> CodeBlock (identity, classes, [("caption", escape f)]) <$> readFile f
-     Nothing -> return cb
+  case lookup "include" namevals of
+   Just f  -> CodeBlock (identity, classes, [("caption", escape f)]) <$> readFile f
+   Nothing -> return cb
   where escape name = foldr (\s a -> (if elem s special then ['\\', s] else [s]) <> a) "" name
         special = "_"
 include x = return x
